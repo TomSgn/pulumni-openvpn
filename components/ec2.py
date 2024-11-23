a67084b1ef4bb0ca485637d93bb0cf46e921de64
Tom <62591045+TomSgn@users.noreply.github.com>
2024-11-23 13:54:15 +0000

    Create gactions

--- components/ec2.py
+++ components/ec2.py
@@ -11,37 +11,38 @@ class EC2Instance:
             key_name=key_name,
             tags={"Name": f"{name}-ec2"},
             user_data="""#!/bin/bash
-yum update -y
-yum install -y openvpn easy-rsa
-mkdir -p /etc/openvpn/easy-rsa/keys
-cp -r /usr/share/easy-rsa/3/* /etc/openvpn/easy-rsa/
-cd /etc/openvpn/easy-rsa
+            yum update -y
+            yum install -y openvpn easy-rsa
+            mkdir -p /etc/openvpn/easy-rsa/keys
+            cp -r /usr/share/easy-rsa/3/* /etc/openvpn/easy-rsa/
+            cd /etc/openvpn/easy-rsa
 
-./easyrsa init-pki
-echo -e "yes" | ./easyrsa build-ca nopass
-./easyrsa gen-req server nopass
-echo -e "yes" | ./easyrsa sign-req server server
-./easyrsa gen-dh
-openvpn --genkey --secret /etc/openvpn/ta.key
+            ./easyrsa init-pki
+            echo -e "yes" | ./easyrsa build-ca nopass
+            ./easyrsa gen-req server nopass
+            echo -e "yes" | ./easyrsa sign-req server server
+            ./easyrsa gen-dh
+            openvpn --genkey --secret /etc/openvpn/ta.key
 
-cat <<EOF > /etc/openvpn/server.conf
-port 1194
-proto udp
-dev tun
-ca /etc/openvpn/easy-rsa/pki/ca.crt
-cert /etc/openvpn/easy-rsa/pki/issued/server.crt
-key /etc/openvpn/easy-rsa/pki/private/server.key
-dh /etc/openvpn/easy-rsa/pki/dh.pem
-server 10.8.0.0 255.255.255.0
-keepalive 10 120
-persist-key
-persist-tun
-status /var/log/openvpn-status.log
-log /var/log/openvpn.log
-verb 3
-EOF
+            cat <<EOF > /etc/openvpn/server.conf
+            port 1194
+            proto udp
+            dev tun
+            ca /etc/openvpn/easy-rsa/pki/ca.crt
+            cert /etc/openvpn/easy-rsa/pki/issued/server.crt
+            key /etc/openvpn/easy-rsa/pki/private/server.key
+            dh /etc/openvpn/easy-rsa/pki/dh.pem
+            server 10.8.0.0 255.255.255.0
+            keepalive 10 120
+            persist-key
+            persist-tun
+            status /var/log/openvpn-status.log
+            log /var/log/openvpn.log
+            verb 3
+            EOF
 
-systemctl enable openvpn@server
-systemctl start openvpn@server
-"""
+            systemctl enable openvpn@server
+            systemctl start openvpn@server
+            """,
+instance_initiated_shutdown_behavior="stop",
         )
